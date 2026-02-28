11c9bbaff97d3e8749d643f55c908763d1619fc2
YourGitHubUsername <sudharshanias@gmail.com>
2026-02-28 10:40:21 +0530

    UC2: Render OOPS banner using print statements

---
+++ OOPSBannerApp.java
@@ -0,0 +1,26 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Row 1
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+
+        // Row 2
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
+
+        // Row 3
+        System.out.println("**     **" + "**     **" + "**   **  " + "**       ");
+
+        // Row 4
+        System.out.println("**     **" + "**     **" + "******   " + "  *****  ");
+
+        // Row 5
+        System.out.println("**     **" + "**     **" + "**       " + "       **");
+
+        // Row 6
+        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+
+        // Row 7
+        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
+    }
+}
