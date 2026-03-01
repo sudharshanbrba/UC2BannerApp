a77eebe9607690a149619b7e46411fdb8ecbc8cb
YourGitHubUsername <sudharshanias@gmail.com>
2026-03-01 15:31:16 +0530

    UC4: Refactored using String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,46 +2,56 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ",
+        // Create String array to store 7 banner lines
+        String[] lines = new String[7];
+
+        // Populate each line using String.join()
+
+        lines[0] = String.join(" ",
                 " ***   ",
                 " ***   ",
                 " ****  ",
-                " ***** "));
+                " ***** ");
 
-        System.out.println(String.join(" ",
+        lines[1] = String.join(" ",
                 "**   ** ",
                 "**   ** ",
                 "**   ** ",
-                "**      "));
+                "**      ");
 
-        System.out.println(String.join(" ",
+        lines[2] = String.join(" ",
                 "**   ** ",
                 "**   ** ",
                 "****    ",
-                " ****   "));
+                " ****   ");
 
-        System.out.println(String.join(" ",
+        lines[3] = String.join(" ",
                 "**   ** ",
                 "**   ** ",
                 "**       ",
-                "     **  "));
+                "     **  ");
 
-        System.out.println(String.join(" ",
+        lines[4] = String.join(" ",
                 "**   ** ",
                 "**   ** ",
                 "**       ",
-                "     **  "));
+                "     **  ");
 
-        System.out.println(String.join(" ",
+        lines[5] = String.join(" ",
                 "**   ** ",
                 "**   ** ",
                 "**   ** ",
-                "**   **  "));
+                "**   **  ");
 
-        System.out.println(String.join(" ",
+        lines[6] = String.join(" ",
                 " ***   ",
                 " ***   ",
                 "****   ",
-                " ***** "));
+                " ***** ");
+
+        // Enhanced for-loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
