72d51aac955865a61cd6d2a940a73d40a78eb130
YourGitHubUsername <sudharshanias@gmail.com>
2026-03-01 15:10:12 +0530

    UC3: Refactored banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,25 +2,46 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Row 1
-        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+        System.out.println(String.join(" ",
+                " ***   ",
+                " ***   ",
+                " ****  ",
+                " ***** "));
 
-        // Row 2
-        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
+        System.out.println(String.join(" ",
+                "**   ** ",
+                "**   ** ",
+                "**   ** ",
+                "**      "));
 
-        // Row 3
-        System.out.println("**     **" + "**     **" + "**   **  " + "**       ");
+        System.out.println(String.join(" ",
+                "**   ** ",
+                "**   ** ",
+                "****    ",
+                " ****   "));
 
-        // Row 4
-        System.out.println("**     **" + "**     **" + "******   " + "  *****  ");
+        System.out.println(String.join(" ",
+                "**   ** ",
+                "**   ** ",
+                "**       ",
+                "     **  "));
 
-        // Row 5
-        System.out.println("**     **" + "**     **" + "**       " + "       **");
+        System.out.println(String.join(" ",
+                "**   ** ",
+                "**   ** ",
+                "**       ",
+                "     **  "));
 
-        // Row 6
-        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+        System.out.println(String.join(" ",
+                "**   ** ",
+                "**   ** ",
+                "**   ** ",
+                "**   **  "));
 
-        // Row 7
-        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
+        System.out.println(String.join(" ",
+                " ***   ",
+                " ***   ",
+                "****   ",
+                " ***** "));
     }
 }
